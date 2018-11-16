4bc168da9d84dd8a07e359ba32cefe264e1e1276
piecesXu <pieces1994212@163.com>
2018-11-16 15:12:57 +0800

    fix bug

--- src/main/java/com/pieces/spaserver/aop/log/LoggingAspect.java
+++ src/main/java/com/pieces/spaserver/aop/log/LoggingAspect.java
@@ -9,7 +9,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
-import org.springframework.web.bind.annotation.RequestAttribute;
 import org.springframework.web.context.request.RequestAttributes;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
